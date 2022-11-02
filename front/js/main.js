adb7279fe6cfedd2948b63655aa3fefedc0c0e1f
kulakovskyi <torchok9@gmail.com>
2022-11-02 11:32:52 +0200

    first

---
+++ front/js/main.js
@@ -0,0 +1,8 @@
+
+(function(){
+    let screenWidth = window.innerWidth
+    if (screenWidth >= 1280){
+        let scene = document.getElementById('scene')
+        let parallaxInstance = new Parallax(scene)
+    }
+}());
